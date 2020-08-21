101e51af5ee35a06fde5a01f79336390b2a73557
ndkhoa0704 <58621544+ndkhoa0704@users.noreply.github.com>
2020-08-21 22:32:30 +0700

    Update components for favorite dish

--- sales/order.hpp
+++ sales/order.hpp
@@ -9,9 +9,10 @@ using namespace std;
 class order
 {
 private:
-    order(){};
+    order();
     static order *instance;
     vector<bill *> bills;
+    vector<int> quantity;
 
 public:
     ~order();
@@ -19,6 +20,9 @@ public:
     order &operator=(const order &other) = delete;
     order *instantiate();
     void NewOrder();
+    void UpdateDishQuant(const int &index, const int &mode);
+    void update(const int &pos);
+    const vector<int> &getOrderedDishQuantity();
 };
 
 #endif
