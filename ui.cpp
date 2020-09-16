0b8c86820b699e3d9f701e70dc1d32da23d9e8e6
manhvu0803 <31726319+manhvu0803@users.noreply.github.com>
2020-09-16 11:56:59 +0700

    Add stream param for input()

--- ui.cpp
+++ ui.cpp
@@ -5,73 +5,75 @@
 #include <limits>
 
 using namespace std;
-using namespace ui;
 
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
-    #include <direct.h>
-    #include <windows.h>
-    
-    void clearConsole()
-    {
-        COORD topLeft  = {0, 0};
-        HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-        CONSOLE_SCREEN_BUFFER_INFO screen;
-        DWORD written;
-
-        GetConsoleScreenBufferInfo(console, &screen);
-        FillConsoleOutputCharacterA(
-            console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
-        );
-        FillConsoleOutputAttribute(
-            console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
-            screen.dwSize.X * screen.dwSize.Y, topLeft, &written
-        );
-        SetConsoleCursorPosition(console, topLeft);
-    }
-#else
-    void clearConsole() {}
-#endif // WIN32
-
-component::component(const string& desc): description(desc) {}
-
-void component::add(component& comp)
+namespace ui
 {
-    components.push_back(&comp);
-}
-
-void component::show()
-{
-    int choice;
-    do {
-        if (clearOnShow) clearConsole();
-        int choiceCnt = components.size();
-
-        cout << "0: Return\n";
-        for (int i = 0; i < choiceCnt; ++i)
-            cout << i + 1  << ": " << components[i]->description << '\n';
-
-        cout << "Your choice: ";
-        while (!input(cin , choice) || choice < 0 || choice > choiceCnt) 
-            cout << "Invalid input, please choose again\n";
+    #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+        #include <direct.h>
+        #include <windows.h>
         
-        if (choice > 0) components[choice - 1]->show();        
+        void clearConsole()
+        {
+            COORD topLeft  = {0, 0};
+            HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+            CONSOLE_SCREEN_BUFFER_INFO screen;
+            DWORD written;
+
+            GetConsoleScreenBufferInfo(console, &screen);
+            FillConsoleOutputCharacterA(
+                console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+            );
+            FillConsoleOutputAttribute(
+                console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
+                screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+            );
+            SetConsoleCursorPosition(console, topLeft);
+        }
+    #elif
+        void clearConsole() {}
+    #endif // WIN32
+
+    component::component(const string& desc): description(desc) {}
+
+    void component::add(component& comp)
+    {
+        components.push_back(&comp);
     }
-    while (choice > 0);    
-}
 
-// option
-option::option(std::function<void()> func): action(func) {}
+    void component::show()
+    {
+        int choice;
+        do {
+            if (clearOnShow) clearConsole();
+            int choiceCnt = components.size();
 
-option::option(std::function<void()> func, const std::string& desc):
-    action(func),
-    component(desc)
-{}
+            cout << "0: Return\n";
+            for (int i = 0; i < choiceCnt; ++i)
+                cout << i + 1  << ": " << components[i]->description << '\n';
 
-void option::show()
-{
-    action();
-    if (successMessage.length() > 0) {
-        cout << successMessage << '\n';
-        cin.get();
+            cout << "Your choice: ";
+            while (!input(choice) || choice < 0 || choice > choiceCnt) 
+                cout << "Invalid input, please choose again\n";
+            
+            if (choice > 0) components[choice - 1]->show();        
+        }
+        while (choice > 0);    
+    }
+
+    // option
+    option::option(std::function<void()> func): action(func) {}
+
+    option::option(std::function<void()> func, const std::string& desc):
+        action(func),
+        component(desc)
+    {}
+
+    void option::show()
+    {
+        action();
+        if (successMessage.length() > 0) {
+            cout << successMessage << '\n';
+            cin.get();
+        }
     }
 }
